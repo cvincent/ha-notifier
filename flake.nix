690231948256ed8281be6454141ba8736610698e
Chris Vincent <chris.vincent@hey.com>
2024-11-24 00:38:10 -0600

    Try flake-parts

--- flake.nix
+++ flake.nix
@@ -3,85 +3,102 @@ rec {
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
   outputs =
-    { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } (
       let
         pname = "ha-notifier";
         version = "0.1.0";
-        pkgs = import nixpkgs { inherit system; };
-        beam-pkg = pkgs.beam.packagesWith pkgs.erlang_26;
-        elixir = beam-pkg.elixir_1_17;
-        mixRelease = beam-pkg.mixRelease.override { inherit elixir; };
-        elixir-ls = pkgs.elixir-ls.override { inherit elixir mixRelease; };
       in
-      rec {
-        devShells.default = pkgs.mkShell {
-          buildInputs = [
-            elixir
-            elixir-ls
-          ];
+      {
+        perSystem =
+          { pkgs, ... }:
+          (
+            let
+              beam-pkg = pkgs.beam.packagesWith pkgs.erlang_26;
+              elixir = beam-pkg.elixir_1_17;
+              mixRelease = beam-pkg.mixRelease.override { inherit elixir; };
+              elixir-ls = pkgs.elixir-ls.override { inherit elixir mixRelease; };
+            in
+            {
+              devShells.default = pkgs.mkShell {
+                buildInputs = [
+                  elixir
+                  elixir-ls
+                ];
 
-          shellHook = ''
-            echo "$(elixir --version | tr -s '\n')"
-            echo ""
-            echo "LFG."
-          '';
-        };
-
-        packages.default = mixRelease (
-          let
-            src = ./.;
-
-            mixFodDeps = beam-pkg.fetchMixDeps {
-              pname = "mix-deps-${pname}";
-              inherit src version;
-              hash = "sha256-qjj1ZAGP4+QXbQTeXYb5IU8Xlhqt9WXsuWuwgCT1Hsk=";
-            };
-          in
-          {
-            inherit
-              src
-              pname
-              version
-              mixFodDeps
-              ;
-          }
-        );
-
-        homeManagerModules.default =
-          { config, lib, ... }:
-          let
-            cfg = config.services.${pname};
-          in
-          {
-            options.services.${pname} = {
-              enable = lib.mkEnableOption pname;
-              port = lib.mkOption {
-                type = lib.types.port;
-                default = 8124;
-                description = "Port to listen for Home Assistant via the REST integration.";
+                shellHook = ''
+                  echo "$(elixir --version | tr -s '\n')"
+                  echo ""
+                  echo "LFG."
+                '';
               };
-            };
 
-            config = lib.mkIf cfg.enable {
-              systemd.user.services.${pname} = {
-                Unit.Description = description;
-                Install.WantedBy = [ "multi-user.target" ];
-                Service.ExecStart = "${pkgs.writeShellScript "ha-notifier" ''
-                  #!/run/current-system/sw/bin/bash
-                  export RELEASE_DISTRIBUTION=none
-                  export RELEASE_COOKIE=$(tr -dc A-Za-z0-9 < /dev/urandom | head -c 20)
-                  export PORT=${toString cfg.port}
-                  ${packages.default}/bin/ha_notifier start
-                ''}";
+              packages.default = mixRelease (
+                let
+                  src = ./.;
+
+                  mixFodDeps = beam-pkg.fetchMixDeps {
+                    pname = "mix-deps-${pname}";
+                    inherit src version;
+                    hash = "sha256-qjj1ZAGP4+QXbQTeXYb5IU8Xlhqt9WXsuWuwgCT1Hsk=";
+                  };
+                in
+                {
+                  inherit
+                    src
+                    pname
+                    version
+                    mixFodDeps
+                    ;
+                }
+              );
+            }
+          );
+
+        flake =
+          { pkgs, self, ... }:
+          {
+            homeManagerModules.default =
+              { config, lib, ... }:
+              let
+                cfg = config.services.${pname};
+              in
+              {
+                options.services.${pname} = {
+                  enable = lib.mkEnableOption pname;
+                  port = lib.mkOption {
+                    type = lib.types.port;
+                    default = 8124;
+                    description = "Port to listen for Home Assistant via the REST integration.";
+                  };
+                };
+
+                config = lib.mkIf cfg.enable {
+                  systemd.user.services.${pname} = {
+                    Unit.Description = description;
+                    Install.WantedBy = [ "multi-user.target" ];
+                    Service.ExecStart = "${pkgs.writeShellScript "ha-notifier" ''
+                      #!/run/current-system/sw/bin/bash
+                      export RELEASE_DISTRIBUTION=none
+                      export RELEASE_COOKIE=$(tr -dc A-Za-z0-9 < /dev/urandom | head -c 20)
+                      export PORT=${toString cfg.port}
+                      ${self.packages.default}/bin/ha_notifier start
+                    ''}";
+                  };
+                };
               };
-            };
           };
+
+        systems = [
+          "x86_64-linux"
+          "aarch64-linux"
+          "aarch64-darwin"
+          "x86_64-darwin"
+        ];
       }
     );
 }
